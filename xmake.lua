3ef9d1cbc5b07e557b15b0177b9657e223f83a2f
Hato1125 <hato.flavius@gmail.com>
2026-08-27 17:53:51 +0900

    build: remove compile_commands generation option

--- xmake.lua
+++ xmake.lua
@@ -14,17 +14,6 @@ package_end()
 add_requires('xdgcpp')
 add_requires('toml++', { system = false })
 
-option('compile_commands')
-  set_default(false)
-  set_showmenu(true)
-  set_description('Generate compile_commands.json')
-option_end()
-
-if has_config('compile_commands') then
-  ---@diagnostic disable-next-line: param-type-mismatch
-  add_rules('plugin.compile_commands.autoupdate', { outputdir = 'build' })
-end
-
 set_allowedplats('linux')
 set_languages('c++26')
 
